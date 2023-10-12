c8ad0d03d0a82139260462503fea39ef783e3633
Hawnzznn <146906178+Hawnzznn@users.noreply.github.com>
2023-10-12 17:58:59 +0700

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './header'; 
+import Footer from './Footer'; 
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Main Content</h1>
+        <p>ini main</p>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
